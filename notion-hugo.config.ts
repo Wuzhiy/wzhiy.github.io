5039dc921702fcf26decfaaab20adaa49459cb73
Wzhiy <61570960+Wuzhiy@users.noreply.github.com>
2023-08-18 01:21:32 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://tree-beef-6a8.notion.site/Notion-DoIt-60c43509117a483f858dafa2f5051820?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
